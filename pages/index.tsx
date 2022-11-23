d76864b1c32fd4c669bc05b324891ce212be1ee7
KOWOONLEE <hhh3901@gmail.com>
2022-11-23 23:24:19 +0900

    Rev :동영상 링크 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,18 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import Seo from "../components/Seo";
 import styles from "../styles/Home.module.css";
+// import Primera from "../public/videos/primera.mp4";
+
+interface HTMLIFrameElement extends HTMLElement {
+  align: string;
+  allowFullscreen: boolean;
+  allowPaymentRequest: boolean;
+  border: string;
+  readonly contentDocument: Document;
+  readonly contentWindow: Window;
+  frameBorder: string;
+  frameSpacing: any;
+}
 
 export default function Home() {
   return (
@@ -11,13 +23,18 @@ export default function Home() {
       <Seo />
       <NavBar />
       <main className={styles.main}>
-        <iframe
-          className="videoMain"
-          src="https://a01-g-naver-vod.pstatic.net/shopnseller/a/read/v2/VOD_ALPHA/shopnseller_2022_10_26_0/821b5d08-54d2-11ed-80e6-a0369ffdd624.mp4?__gda__=1669153037_efec5d3fbdc24eb75fa03ddf9a346e62"
-          title="video player"
-          frameborder="0"
-          allowfullscreen
-        ></iframe>
+        <div className="videoBox">
+          <iframe
+            className="videoMain"
+            src="/videos/primera.mp4"
+            title="video player"
+            frameBorder="0"
+            allowFullScreen={true}
+            width="400"
+            height="225"
+          ></iframe>
+        </div>
+
         {/* <video
           playsinline
           disablepictureinpicture="true"
@@ -34,9 +51,19 @@ export default function Home() {
         .container {
           height: 100%;
         }
+        .videoBox {
+          position: relative;
+          padding-top: 30px;
+          padding-bottom: 56.25%;
+          height: 0;
+          overflow: hidden;
+          width: 100%;
+          height: 100%;
+        }
         .videoMain {
           position: absolute;
           left: 0;
+          top: 0;
           width: 100%;
           height: 100%;
         }
